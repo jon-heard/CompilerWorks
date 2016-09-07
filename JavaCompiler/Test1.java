3df59ca574f2ff0435fa203e4322f04c53584d1e
Jon Heard <nethermonkey@gmail.com>
2016-09-07 12:24:34 -0400

    - Added test files to .ignore

--- JavaCompiler/Test1.java
+++ JavaCompiler/Test1.java
@@ -1,11 +1,10 @@
+package first.second;
 import java.util.*;
 import javax.swing.JLabel;
 public class Test1
 {
 	public static void main(String[] args)
 	{
-//		Object c = 1_2;
-//		System.out.println(c.getClass());
-		System.out.println(0X5.5);
+		System.out.println("Hello world");
 	}
 }
