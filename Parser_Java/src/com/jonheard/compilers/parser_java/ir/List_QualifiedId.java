b94e86f6c9f0d9cd04df11c392d3371b0b3f7f24
jon-heard <jon.heard.tutor@gmail.com>
2017-06-01 11:36:03 -0400

    Parser: Added 'List_QualifiedId' for implements clause

---
+++ Parser_Java/src/com/jonheard/compilers/parser_java/ir/List_QualifiedId.java
@@ -0,0 +1,43 @@
+
+package com.jonheard.compilers.parser_java.ir;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jonheard.compilers.parser_java.Parser;
+import com.jonheard.compilers.tokenizer_java.TokenType;
+
+public class List_QualifiedId extends BaseIrType {
+  public List_QualifiedId(Parser parser) {
+    this(parser, false);
+  }
+  public List_QualifiedId(Parser parser, boolean isEmpty) {
+    super(parser);
+    if (!isEmpty) {
+      do {
+        addChild(new QualifiedId(parser));
+      } while (parser.passTokenIfType(TokenType.COMMA));
+    }
+  }
+  
+  public String toString() {
+    StringBuilder result = new StringBuilder();
+    String prefix = "";
+    List<QualifiedId> ids = getValue();
+    for (QualifiedId id : ids) {
+      result.append(prefix);
+      result.append(id.getValue());
+      prefix = " ";
+    }
+    return result.toString();
+  }
+  
+  public List<QualifiedId> getValue() {
+    ArrayList<QualifiedId> result = new ArrayList<>();
+    int childCount = getChildCount();
+    for (int i = 0; i < childCount; ++i) {
+      result.add((QualifiedId)getChild(i));
+    }
+    return result;
+  }
+}
