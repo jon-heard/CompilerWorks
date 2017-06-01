873f607b56b4b35d26006e870fa76eea607aed9c
jon-heard <jon.heard.tutor@gmail.com>
2017-06-01 04:22:53 -0400

    Parser: Added 'basic for' parsing

--- Parser_Java/src/com/jonheard/compilers/parser_java/ir/statement/For.java
+++ Parser_Java/src/com/jonheard/compilers/parser_java/ir/statement/For.java
@@ -3,6 +3,9 @@ package com.jonheard.compilers.parser_java.ir.statement;
 
 import com.jonheard.compilers.parser_java.Parser;
 import com.jonheard.compilers.parser_java.ir.BaseIrType;
+import com.jonheard.compilers.parser_java.ir.List_Expression;
+import com.jonheard.compilers.parser_java.ir.Member;
+import com.jonheard.compilers.parser_java.ir.expression.ExpressionParser;
 import com.jonheard.compilers.tokenizer_java.TokenType;
 
 public class For extends BaseIrType {
@@ -10,9 +13,16 @@ public class For extends BaseIrType {
     super(parser);
     parser.requireTokenToBeOfType(TokenType._FOR);
     parser.requireTokenToBeOfType(TokenType.LEFT_PAREN);
-    while (!parser.passTokenIfType(TokenType.RIGHT_PAREN)) {
-      parser.getTokenQueue().poll();
+    if (Member.getIsNext(parser)) {
+      addChild(new Member(parser, true, true, false, false));
+    } else {
+      addChild(new List_Expression(parser));
+      parser.requireTokenToBeOfType(TokenType.SEMICOLON);
     }
+    addChild(ExpressionParser.parseExpressionStatment(parser));
+    parser.requireTokenToBeOfType(TokenType.SEMICOLON);
+    addChild(new List_Expression(parser));
+    parser.requireTokenToBeOfType(TokenType.RIGHT_PAREN);
     addChild(StatementParser.getNext(parser));
   }
 
